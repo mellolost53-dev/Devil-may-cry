6650fbb806d0b56929aacc5e7031d7fbbd9095f2
mellolost53-dev <mellolost53@gmail.com>
2026-03-16 19:08:17 +0200

    Fix conflicts and switch from LFS

--- js/loader.js
+++ js/loader.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 window.addEventListener('load', () => {
     const loader = document.getElementById("loader");
     if (loader) {
@@ -11,4 +12,19 @@ window.addEventListener('load', () => {
             }, 1200); 
         }, 2000);
     }
+=======
+window.addEventListener('load', () => {
+    const loader = document.getElementById("loader");
+    if (loader) {
+        setTimeout(() => {
+            loader.classList.add("loader-hidden"); 
+            // початок анімації
+            
+            // після анімки завершується елемент
+            setTimeout(() => {
+                loader.style.display = "none";
+            }, 1200); 
+        }, 2000);
+    }
+>>>>>>> 3511ea7 (Initial commit with LFS assets)
 });
--- js/navigation.js
+++ js/navigation.js
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 document.querySelectorAll(".hero-character").forEach(card => {
   card.addEventListener("click", () => {
     const target = card.dataset.target;
     window.location.href = `characters.html#${target}`;
   });
 });
+=======
+document.querySelectorAll(".hero-character").forEach(card => {
+  card.addEventListener("click", () => {
+    const target = card.dataset.target;
+    window.location.href = `characters.html#${target}`;
+  });
+});
+>>>>>>> 3511ea7 (Initial commit with LFS assets)
 //події для карток
